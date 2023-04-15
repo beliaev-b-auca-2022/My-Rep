407974c2f72fa9fc8142e6cef447c89520193e91
Fridge <fridge.1002@gmail.com>
2023-04-15 21:46:38 +0600

    URI1248 Accepted

---
+++ uri-midterm/src/URI1248.java
@@ -0,0 +1,56 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class URI1248 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine(); // consume the leftover newline character
+
+        for (int t = 0; t < n; t++) {
+            String diet = in.nextLine();
+            String breakfast = in.nextLine();
+            String lunch = in.nextLine();
+            Map<Character, Integer> dietMap = new HashMap<>();
+            Map<Character, Integer> consumedMap = new HashMap<>();
+            for (char c : diet.toCharArray()) {
+                dietMap.put(c, dietMap.getOrDefault(c, 0) + 1);
+            }
+            for (char c : breakfast.toCharArray()) {
+                if (dietMap.containsKey(c)) {
+                    consumedMap.put(c, consumedMap.getOrDefault(c, 0) + 1);
+                }
+            }
+            for (char c : lunch.toCharArray()) {
+                if (dietMap.containsKey(c)) {
+                    consumedMap.put(c, consumedMap.getOrDefault(c, 0) + 1);
+                }
+            }
+
+            // check if any character in the diet is not consumed or over-consumed
+            boolean cheated = false;
+            StringBuilder dinner = new StringBuilder();
+            for (Map.Entry<Character, Integer> entry : dietMap.entrySet()) {
+                char c = entry.getKey();
+                int count = entry.getValue();
+                int consumedCount = consumedMap.getOrDefault(c, 0);
+                if (consumedCount == 0 || consumedCount > count) {
+                    cheated = true;
+                    break;
+                }
+                for (int i = 0; i < count - consumedCount; i++) {
+                    dinner.append(c);
+                }
+            }
+
+            // print the result
+            if (cheated) {
+                System.out.println("CHEATER");
+            } else {
+                System.out.println(dinner.toString());
+            }
+        }
+        in.close();
+    }
+}
